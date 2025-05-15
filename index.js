bfd8aaba0583e24c5002ae640d2a293b33d6ab60
Flavio Ferreira <flavio_luiz_ferreira@hotmail.com>
2025-05-15 11:41:06 -0300

    set PORT to use environment variable or default to 3001

--- index.js
+++ index.js
@@ -195,6 +195,6 @@ app.delete('/api/notes/:id', (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
